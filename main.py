dc560faaabcac31db81779c02cf35afd46788b98
jose-omar-landa <jlanda4@wgu.edu>
2023-11-08 16:39:54 -0600

    optimized code by creating functions to read and write to the todolist txt file

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
+def get_todos(filepath):
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(filepath, todos_arg):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ""Type add, show, or exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case "add":
-            todo = input("Enter an item for your todo list: ") + "\n"
+    if user_action.startswith("add"):
+        todo = user_action[4:]
 
-            with open('todo_list.txt', "r") as file:
-                todos = file.readlines()
+        todos = get_todos('todo_list.txt')
 
-            todos.append(todo.title())
+        todos.append(todo.title() + '\n')
 
-            with open('todo_list.txt', 'w') as file:
-                file.writelines(todos)
+        write_todos('todo_list.txt', todos)
 
-            print("Item added!")
+        print("Item added!")
 
-        case "show":
+    elif user_action.startswith("show"):
 
-            with open('todo_list.txt', "r") as file:
-                todos = file.readlines()
+        todos = get_todos('todo_list.txt')
 
-            for index, item in enumerate(todos):
-                item = item.strip("\n")
-                row = f"{index + 1}. {item}"
-                print(row)
-        case "edit":
-            todo_item_num = int(input("Enter the todo list item number you'd like to edit: "))
+        for index, item in enumerate(todos):
+            item = item.strip("\n")
+            row = f"{index + 1}. {item}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:
+            todo_item_num = int(user_action[5:])
             todo_item_num = todo_item_num - 1
 
-            with open('todo_list.txt', "r") as file:
-                todos = file.readlines()
+            todos = get_todos('todo_list.txt')
 
             new_item = input("Enter a new todo list item: ")
             todos[todo_item_num] = new_item.title() + "\n"
             print("Item edited.")
 
-            with open('todo_list.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos('todo_list.txt', todos)
 
-            with open('todo_list.txt', "r") as file:
-                todos = file.readlines()
+            todos = get_todos('todo_list.txt')
 
             print("Todo list is now: ")
             for index, item in enumerate(todos):
@@ -48,21 +54,22 @@ while True:
                 row = f"{index + 1}. {item}"
                 print(row)
 
-        case "complete":
-            list_item_num = int(input("Enter the todo list item number you'd like to complete: "))
+        except ValueError:
+            print("Input not valid. Please enter the todo item number to edit.")
 
-            with open('todo_list.txt', "r") as file:
-                todos = file.readlines()
+    elif user_action.startswith("complete"):
+        try:
+            list_item_num = int(user_action[9:])
+
+            todos = get_todos('todo_list.txt')
 
             todos.pop(list_item_num - 1)
 
-            with open('todo_list.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos('todo_list.txt', todos)
 
             print("Item completed!")
 
-            with open('todo_list.txt', "r") as file:
-                todos = file.readlines()
+            todos = get_todos('todo_list.txt')
 
             print("Todo list is now: ")
             for index, item in enumerate(todos):
@@ -70,8 +77,14 @@ while True:
                 row = f"{index + 1}. {item}"
                 print(row)
 
-        case "exit":
-            print("Goodbye!")
-            break
+        except ValueError:
+            print("Input not valid. Please enter the todo item number to complete.")
+
+    elif user_action.startswith("exit"):
+        print("Goodbye!")
+        break
+
+    else:
+        print("Input is invalid, please enter a valid command!")
 
 
