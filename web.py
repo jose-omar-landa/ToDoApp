5f7376dfe6bf6351ede52f1b226f3784024936d3
jose-omar-landa <jlanda4@wgu.edu>
2023-11-11 16:08:26 -0600

    implemented functionality to complete items from the todo list by clicking the checkbox

--- web.py
+++ web.py
@@ -10,13 +10,16 @@ def add_todo():
 
 todos = functions.get_todos()
 
-st.title("My Todo App")
-st.subheader("This is a todo list app")
-st.write("This app is to increase your productivity")
+st.title("Your To-Do List")
+st.subheader("")
 
-for todo in todos:
-    st.checkbox(todo)
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add a new item...", on_change=add_todo, key="new_todo")
-
-st.session_state
